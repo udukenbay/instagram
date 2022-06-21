806f24cbdb1ea72ee3b1efd88047a18075cad2da
u.dukenbay <urziya.dukenbay@gmail.com>
2022-06-21 23:14:02 +0600

    add the menu and profile image at the right of the header component

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import React from 'react';
-import { SearchIcon } from '@heroicons/react/outline';
+import { SearchIcon, PlusCircleIcon } from '@heroicons/react/outline';
+import { HomeIcon } from '@heroicons/react/solid';
 
 export default function Header() {
   return (
 
-        <div className='flex items-center justify-between max-w-6xl'>
+        <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
             {/* Left */}
             <div className='cursor-pointer  h-24 w-24 relative hidden lg:inline-grid'>
                 <Image
@@ -32,7 +33,11 @@ export default function Header() {
             </div>
     
             {/* Right */}
-            <h1>Right sides</h1>
+            <div className='flex space-x-4 items-center'>
+                <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+                <PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+                <img src='https://yt3.ggpht.com/ytc/AKedOLTu3R-kFSG843RnmFSq_Pt5vBhc6l4SSJWalkWKaA=s900-c-k-c0x00ffffff-no-rj' alt='user-image' className='h-10 rounded-full cursot-pointer'/>
+            </div>
         </div>
 
   )
