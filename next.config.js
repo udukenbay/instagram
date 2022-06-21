8d6693185a50fb2d29c3e282b525e0610e5ce280
u.dukenbay <urziya.dukenbay@gmail.com>
2022-06-21 21:35:06 +0600

    add instagram logo to the left side of the header component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ["www.jennexplores.com", "upload.wikimedia.org"],
+  },
 }
-
-module.exports = nextConfig
