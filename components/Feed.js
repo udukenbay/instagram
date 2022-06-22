ed808e8180417cbc842ebc0189267d292fb47ce7
u.dukenbay <urziya.dukenbay@gmail.com>
2022-06-22 20:54:14 +0600

    make the feed section responsive

--- components/Feed.js
+++ components/Feed.js
@@ -2,8 +2,8 @@ import Posts from "./Posts";
 import Stories from "./Stories";
 
 export default function Feed() {
-  return <main>
-      <section>
+  return <main className="grid grid-cols-1 md:grid-cols-3 md:max-w6xl mx-auto">
+      <section className="md:col-span-2">
           {/* Stories */}
           <Stories/>
 
@@ -11,7 +11,7 @@ export default function Feed() {
           <Posts/>
       </section>
 
-      <section>
+      <section className="hidden md:inline-grid md:col-span-1">
           {/* Mini Profile */}
 
           {/* Suggestions */}
--- components/Posts.js
+++ components/Posts.js
@@ -8,14 +8,14 @@ export default function Posts() {
             username: "uad.uad",
             userImg: "https://yt3.ggpht.com/ytc/AKedOLTu3R-kFSG843RnmFSq_Pt5vBhc6l4SSJWalkWKaA=s900-c-k-c0x00ffffff-no-rj",
             img: "https://images.unsplash.com/photo-1655803230295-63df4db6cfac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNXx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60",
-            caption: "Sunset in the city"
+            caption: "Sunset in the city 🌆"
         },
         {
             id: "2",
             username: "urziya-dukenbay",
             userImg: "https://i.pinimg.com/280x280_RS/8d/9c/1b/8d9c1b8e648e5630ef4f733a3b9cf66e.jpg",
             img: "https://images.unsplash.com/photo-1551523713-c1473aa01d9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHN1bW1lcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-            caption: "Summer time"
+            caption: "Summer time 🏖"
         }
     ]
   return <div>
