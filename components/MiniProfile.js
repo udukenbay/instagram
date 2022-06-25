6cc85c52902dc690f7e63e34f963f07889372d17
u.dukenbay <urziya.dukenbay@gmail.com>
2022-06-25 12:09:09 +0600

    get the session and modify the header component with google data && modify mini profile and story components to include the session

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,10 +1,14 @@
+import { useSession, signOut } from "next-auth/react";
+
 export default function MiniProfile() {
+    const { data: session } = useSession();
+
   return <div className="flex items-center justify-between mt-14 ml-10">
-      <img className="h-16 rounded-full border p-[2px]" src="https://yt3.ggpht.com/ytc/AKedOLTu3R-kFSG843RnmFSq_Pt5vBhc6l4SSJWalkWKaA=s900-c-k-c0x00ffffff-no-rj" alt="" />
+      <img className="h-16 rounded-full border p-[2px]" src={session?.user.image} alt="" />
       <div className="flex-1 ml-4">
-          <h2 className="font-bold">uad.uad</h2>
+          <h2 className="font-bold">{session?.user.username}</h2>
           <h3 className="text-sm text-gray-400">Welcome to instagram</h3>
       </div>
-      <button className="font-semibold text-blue-400 text-sm">Sign out</button>
+      <button onClick={signOut} className="font-semibold text-blue-400 text-sm">Sign out</button>
   </div>
 }
