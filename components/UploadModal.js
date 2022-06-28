e03650d3f124e34841e87f4359392b2ffc975bb5
u.dukenbay <urziya.dukenbay@gmail.com>
2022-06-28 23:07:03 +0600

    create a post and add it to firestore and get the download url

--- components/UploadModal.js
+++ components/UploadModal.js
@@ -3,11 +3,44 @@ import { useRecoilState } from "recoil";
 import Modal from 'react-modal';
 import { CameraIcon } from "@heroicons/react/outline";
 import { useState, useRef } from "react";
+import { addDoc, collection, doc, serverTimestamp, updateDoc } from "firebase/firestore";
+import { db, storage } from "../firebase";
+import { useSession } from "next-auth/react";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 export default function UploadModal() {
     const [open, setOpen] = useRecoilState(modalState);
     const [selectedFile, setSelectedFile] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const { data: session } = useSession();
     const filePickerRef = useRef(null);
+    const captionRef = useRef(null);
+
+    async function uploadPost() {
+        if(loading) return;
+
+        setLoading(true);
+
+        const docRef = await addDoc(collection(db, "posts"), {
+            caption: captionRef.current.value,
+            username: session.user.username,
+            profileImg: session.user.image,
+            timestamp: serverTimestamp()
+        });
+
+        const imageRef = ref(storage, `posts/${docRef.id}/image`);
+        await uploadString(imageRef, selectedFile, "data_url").then(
+            async(snapshot)=>{
+                const downloadURL = await getDownloadURL(imageRef);
+                await updateDoc(doc(db, "posts", docRef.id), {
+                    image: downloadURL,
+                });
+            }
+        );
+        setOpen(false);
+        setLoading(false);
+        setSelectedFile(null);
+    }
 
     function addImageToPost(event) {
         const reader = new FileReader();
@@ -36,8 +69,17 @@ export default function UploadModal() {
                         <CameraIcon onClick={()=>filePickerRef.current.click()} className="cursor-pointer h-14 bg-red-200 p-2 rounded-full border-2 text-red-500" />
                     )}
                     <input type="file" hidden ref={filePickerRef} onChange={addImageToPost} />
-                    <input type="text" maxLength="150" placeholder="Please enter your caption..." className="m-4 border-none text-center w-full focus:ring-0" />
-                    <button disabled className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100">Upload Post</button>
+                    <input type="text" maxLength="150"
+                        placeholder="Please enter your caption..."
+                        className="m-4 border-none text-center w-full focus:ring-0"
+                        ref={captionRef}
+                    />
+                    <button
+                        disabled={!selectedFile || loading}
+                        onClick={uploadPost}
+                        className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+                    >Upload Post
+                    </button>
                 </div>
           </Modal>
       )}
